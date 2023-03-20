e235798804e94608f4d4af624a08cc7ef987cdd2
Osmair Coelho <osmair.dev@softmus.com.br>
2023-03-20 23:33:14 +0000

    test: Criação de testes unitários.
    Foram criados os testes, porem nao esta 100 por cento coberto, sera feito mais teste.

---
+++ tests/Feature/CRUDTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class CRUDTest extends TestCase
+{
+
+}
